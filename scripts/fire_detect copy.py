45ea1c1fac840d314c48056978bd43ea20942a80
Badikala Susheel Kumar <bsusheelkumar01@gmail.com>
2024-03-23 22:50:06 +0530

    .

--- scripts/fire_detect copy.py
+++ scripts/fire_detect copy.py
@@ -9,63 +9,100 @@ import math
 from geometry_msgs.msg import Twist
 import time
 import RPi.GPIO as GPIO
-
 GPIO.setmode(GPIO.BCM)
 
 # Set pin 23 (modify if using a different pin) as output
-GPIO.setup(23, GPIO.OUT)
-
+GPIO.setup(18, GPIO.OUT)
 class MyNode(Node):
-
-    def  __init__(self):
+    def __init__(self):
         super().__init__("my_node")
         self.get_logger().info("testing cv2")
-        self.model = YOLO('trained.pt')
-        self.camera_sub = self.create_subscription(Image,"image_raw",self.camera_callback,10)
-        self.img_pub = self.create_publisher(Image,"camera_output",1)
-        # self.cmd_vel_pub = self.create_publisher(Twist,"/cmd_vel",10)
-        # self.timer = self.create_timer(0.5,self.send_vel_cmd)
+        self.model = YOLO('/home/robot/project/src/my_robot/trained.pt')
+        self.camera_sub = self.create_subscription(Image, "image_raw", self.camera_callback, 10)
+        self.img_pub = self.create_publisher(Image, "camera_output", 1)
+        self.cmd_vel_pub = self.create_publisher(Twist,"/cmd_vel",10)
+        self.timer = self.create_timer(0.5,self.send_cmd_vel)
         self.class_names = ['fire']
         self.bridge = CvBridge()
-        
+        self.fire_detected = False  # Track if fire is detected
+        self.tolerance = 0.1
+        self.x_deviation = 0
+        self.y_max = 0
 
-    def camera_callback(self,data):
-        img = self.bridge.imgmsg_to_cv2(data,"bgr8")
-        results = self.model(img,stream=True)
+    def send_cmd_vel(self,linear_val=0.0,angular_val=0.0):
+        move = Twist()
+        move.linear.x = linear_val
+        move.angular.x = angular_val
+        self.cmd_vel_pub.publish(move)
+        
+    def camera_callback(self, data):
+    
+        img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        results = self.model(img, stream=True)
+
+        # Reset fire detection flag
+        self.fire_detected = False
+
         for r in results:
             boxes = r.boxes
             for box in boxes:
-                x_min, y_min, x_max, y_max= box.xyxy[0]
+                x_min, y_min, x_max, y_max = box.xyxy[0]
                 x_min, y_min, x_max, y_max = int(x_min), int(y_min), int(x_max), int(y_max)
-                print(x_min, y_min, x_max, y_max)
-                cv2.rectangle(img,(x_min,y_min),(x_max,y_max),(255,0,255),3)
-
-                conf = math.ceil((box.conf[0]*100))  /100
-                tolerance=0.1
-                x_deviation=0
-                y_max=0
-
+                cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (255, 0, 255), 3)
+                conf = math.ceil((box.conf[0] * 100)) / 100
                 cls = int(box.cls[0])
+                x_diff = x_max - x_min
+                y_diff = y_max - y_min
 
+                obj_x_centre = x_min +(x_diff/2)
+                obj_x_centre = round(obj_x_centre,3)
+
+                obj_y_centre = y_min+(y_diff/2)
+                obj_y_centre = round(obj_y_centre,3)
+
+                x_deviation = round(0.5 - obj_x_centre,3)
+                y_max = round(y_max,3)
+
+                print(f" x_deviation {x_deviation}, y_deviation {y_max}")
                 if cls == 0:
-                    cv2.putText(img, f'{self.class_names[cls]} {conf}', (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
-                    x_diff = x_max - x_min
-                    y_diff = y_max - y_min
+                    cv2.putText(img, f'{self.class_names[cls]} {conf}', (x_min, y_min - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
+                    if (self.x_deviation >= self.tolerance):
+                        delay1 = self.get_delay(x_deviation)
+                        self.send_cmd_vel(0.0,-0.5)
+                        time.sleep(delay1)
+                        self.send_cmd_vel(0.0,0.0)
+                    
+                    if(x_deviation <= -1*self.tolerance):
+                        delay1 = self.get_delay(x_deviation)
+                        self.send_cmd_vel(0.0,0.5)
+                        time.sleep(delay1)
+                        self.send_cmd_vel(0.0,0.0)
+                    self.fire_detected = True  # Set flag when fire is detected
+                
 
-                    obj_x_centre = x_min+(x_diff/2)
-                    obj_x_centre = round(obj_x_centre,3)
+        if self.fire_detected:
+            GPIO.output(18, 0)  # Turn on relay
+        else:
+            GPIO.output(18, 1)  # Turn off relay
 
-                    x_deviation = round(0.5-obj_x_centre,3)
-                    y_max = round(y_max,3)
-                    print("{",x_deviation,y_max,"}")
-                    GPIO.output(23,1)
-                else:
-                    GPIO.output(23,0)
-
-        img_to_pub = self.bridge.cv2_to_imgmsg(img,"bgr8")
+        img_to_pub = self.bridge.cv2_to_imgmsg(img, "bgr8")
         self.img_pub.publish(img_to_pub)
-        # cv2.imshow("output",img)
-        cv2.waitKey(1)
+
+    def get_delay(self,deviation):
+        
+        deviation = abs(deviation)
+
+        if (deviation>=0.4):
+            d = 0.080
+        elif(deviation>=0.35 and deviation <0.40):
+            d = 0.060
+        elif(deviation>=0/20 and deviation<0.35):
+            d = 0.050
+        else:
+            d = 0.040
+        return d
+
 def main(args=None):
     rclpy.init(args=args)
     node = MyNode()
---
+++ scripts/sim_single_nav.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+from geometry_msgs.msg import PoseStamped
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
+import rclpy
+from rclpy.duration import Duration
+
+from Adafruit_IO import Client, RequestError, Feed
+from playsound import playsound
+
+# Set to your Adafruit IO key.
+# Remember, your key is a secret,
+# so make sure not to publish it when you publish this code!
+ADAFRUIT_IO_KEY = 'aio_WpIy30mhDyPUUbLtFci1IkD9QPoH'
+
+# Set to your Adafruit IO username.
+# (go to https://accounts.adafruit.com to find your username)
+ADAFRUIT_IO_USERNAME = 'BSusheelKumar'
+
+
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+
+try:
+    smoke = aio.feeds('smoke')
+except RequestError: # Doesn't exist, create a new feed
+    feed = Feed(name="smoke")
+    smoke = aio.create_feed(feed)
+
+
+def main():
+    rclpy.init()
+
+    navigator = BasicNavigator()
+
+    initial_pose = PoseStamped()
+    initial_pose.header.frame_id = 'map'
+    initial_pose.header.stamp = navigator.get_clock().now().to_msg()
+    initial_pose.pose.position.x = -0.29967716336250305
+    initial_pose.pose.position.y = -0.0009105631615966558
+    initial_pose.pose.orientation.z = 0.03092659537290271
+    initial_pose.pose.orientation.w =  0.9995216584439983
+    navigator.setInitialPose(initial_pose)
+
+
+    navigator.waitUntilNav2Active()
+
+    # If desired, you can change or load the map as well
+    # navigator.changeMap('/path/to/map.yaml')
+
+    # You may use the navigator to clear or obtain costmaps
+    # navigator.clearAllCostmaps()  # also have clearLocalCostmap() and clearGlobalCostmap()
+    # global_costmap = navigator.getGlobalCostmap()
+    # local_costmap = navigator.getLocalCostmap()
+
+    # Go to our demos first goal pose
+    goal_pose = PoseStamped()
+    goal_pose.header.frame_id = 'map'
+    goal_pose.header.stamp = navigator.get_clock().now().to_msg()
+    goal_pose.pose.position.x = 5.653504371643066
+    goal_pose.pose.position.y = 2.7832412719726562
+    goal_pose.pose.orientation.w = 0.7205151090486812
+
+    # sanity check a valid path exists
+    # path = navigator.getPath(initial_pose, goal_pose)
+    while True:
+        data = aio.receive(smoke.key)
+        print('Latest value from smoke detector: {0}'.format(data.value))
+        if data.value == '1':
+            playsound("/home/bsusheelkumar/final_year_project/src/my_robot/jaldhi waha se hato.mp3")
+            navigator.goToPose(goal_pose)
+
+            i = 0
+            while not navigator.isTaskComplete():
+                ################################################
+                #
+                # Implement some code here for your application!
+                #
+                ################################################
+
+                # Do something with the feedback
+                i = i + 1
+                feedback = navigator.getFeedback()
+                if feedback and i % 5 == 0:
+                    print('Estimated time of arrival: ' + '{0:.0f}'.format(
+                        Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
+                        + ' seconds.')
+
+                    # Some navigation timeout to demo cancellation
+                    if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
+                        navigator.cancelTask()
+
+                    # Some navigation request change to demo preemption
+                    if Duration.from_msg(feedback.navigation_time) > Duration(seconds=18.0):
+                        goal_pose.pose.position.x != -3.0
+                        navigator.goToPose(goal_pose)
+
+            # Do something depending on the return code
+            result = navigator.getResult()
+            if result == TaskResult.SUCCEEDED:
+                print('Goal succeeded!')
+                navigator.goToPose(initial_pose)
+            elif result == TaskResult.CANCELED:
+                print('Goal was canceled!')
+            elif result == TaskResult.FAILED:
+
+                print('Goal failed!, Going to initial position')
+                navigator.goToPose(initial_pose)
+
+            else:
+                print('Goal has an invalid return status!')
+
+            navigator.lifecycleShutdown()
+
+            exit(0)
+        else:
+            # Wait for a value from the feed
+            while True:
+                # Keep checking for new values
+                new_data = aio.receive(smoke.key)
+                
+                if new_data.value != '1':  # Avoid unnecessary repetition
+                    print('Waiting for a non-1 value from the feed...')
+                else:
+                    print('Received a non-1 value from the feed!')
+                    break  # Exit the inner loop when a non-1 value is received
+
+if __name__ == '__main__':
+    main()
