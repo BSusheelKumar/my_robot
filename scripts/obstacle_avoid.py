ac4685e54435b8f416e8df07ea6d685377302774
Badikala Susheel Kumar <bsusheelkumar01@gmail.com>
2024-03-25 21:21:00 +0530

    .

--- scripts/obstacle_avoid.py
+++ scripts/obstacle_avoid.py
@@ -19,12 +19,19 @@ class MyNode(Node):
         self.image_width = None
         self.k_p = 0.001  # Proportional control gain
         self.linear_speed = 0.2  # Linear velocity
+        self.angular_speed = 0.5  # Angular velocity for rotation
         self.timer = self.create_timer(0.1, self.send_cmd_vel)
+        self.fire_detected = False
 
     def send_cmd_vel(self):
-        if self.target_x is None or self.image_width is None:
-            # Stop the robot if no fire detected
+        if self.fire_detected:
+            # Stop the robot near the fire and face the fire
             self.stop_robot()
+            return
+
+        if self.target_x is None or self.image_width is None:
+            # Rotate the robot if no fire detected
+            self.rotate_robot()
             return
 
         error = self.target_x - self.image_width / 2  # Calculate error (difference from center)
@@ -40,6 +47,12 @@ class MyNode(Node):
         move = Twist()
         self.cmd_vel_pub.publish(move)
 
+    def rotate_robot(self):
+        # Rotate the robot to search for fire
+        move = Twist()
+        move.angular.z = self.angular_speed
+        self.cmd_vel_pub.publish(move)
+
     def camera_callback(self, data):
         img = self.bridge.imgmsg_to_cv2(data, "bgr8")
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -47,9 +60,10 @@ class MyNode(Node):
         if len(fire) > 0:  # If fire detected
             (x, y, w, h) = fire[0]  # Use the first detected fire
             self.target_x = x + w / 2  # Update target x-coordinate (center of the fire)
-            print(f"x{x} y{y}")
+            self.fire_detected = True
         else:
             self.target_x = None  # No fire detected, reset target x-coordinate
+            self.fire_detected = False
         self.image_width = img.shape[1]  # Get image width
 
 def main(args=None):
